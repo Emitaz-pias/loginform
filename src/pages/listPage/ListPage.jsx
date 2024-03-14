d565e707cf1734f2fc9a83fc475002d3c5c8bf5a
emtiazpias <emtiazpias383@gmail.com>
2024-03-14 22:04:14 +0600

    modal added

--- src/pages/listPage/ListPage.jsx
+++ src/pages/listPage/ListPage.jsx
@@ -1,13 +1,24 @@
-import { Box, Grid, Typography } from '@mui/material';
+import { Box, Grid, TextField, Typography } from '@mui/material';
 import React, { useState } from 'react';
 import ReactFlagsSelect from 'react-flags-select';
 import './ListPage.css'
+import rpic from '../../assets/rpic.png'
+import Modal from './Modal'
 
 const ListPage = () => {
     const [select, setSelect] = useState("FR");
     const onSelect = (code) => setSelect(code);
+    const [open, setOpen] =useState(true);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
     return (
-        <Box sx={{pading:'3em 3em',height:'110vh',backgroundColor:"#d8dce3"}}>
+        <Box sx={{pading:'1.5em 2em',height:'115vh',backgroundColor:"#d8dce3"}}>
             <Box sx={{display:'flex',justifyContent:'space-between'}}>
             <Box><a href="/">Page Principal</a></Box>
             <Box>
@@ -27,17 +38,27 @@ const ListPage = () => {
           </Box>
 
             </Box>
-            <Grid container justifyContent={'center'}>
-             <Box marginBottom='2em'>
+           {setOpen?(<Box> <Grid container justifyContent={'center'}>
+             <Box >
              <Grid xs={12} sm={6} md={3} lg={3}>
            <Box className='box'> <Typography>LIST OF PAYMENT QUERIES AGENT</Typography><Typography className='dollar-sign'>$</Typography></Box>
            </Grid>
-           <Grid xs={12} sm={6} md={3} lg={3}>      <Box className='box'><Typography>PAYMENT QUERIES PROCESSING</Typography><Typography className='dollar-sign'>$</Typography></Box>   </Grid>
+           <Grid xs={12} sm={6} md={3} lg={3}>      <Box onClick={handleClickOpen} className='box'><Typography>PAYMENT QUERIES PROCESSING</Typography><Typography className='dollar-sign'>$</Typography></Box>   </Grid>
            <Grid xs={12} sm={6} md={3} lg={3}>         <Box className='box'><Typography>DEMANDES DE RETRAIT</Typography><Typography className='dollar-sign'>$</Typography></Box> </Grid>
            <Grid xs={12} sm={6} md={3} lg={3}>    <Box className='box'><Typography>PENDING WITHDRAWAL REQUESTS</Typography><Typography className='dollar-sign'>$</Typography></Box> 
-           </Grid>  </Box>
+           </Grid>  
+           </Box>
             </Grid>
- 
+            <Grid container justifyContent={'center'} >
+            <img   style={{height:'4em',width:'8em',}} src={rpic} alt="something" />
+            </Grid>
+           
+           <Grid container justifyContent={'center'}>
+           <Box sx={{backgroundColor:"#16acbd",color:'white',textAlign:'center',width:'13em', padding:'0.5em 1em',borderRadius:'0.2rem',fontWeight:"bold"}} >
+              CONNECT TO REDDY
+            </Box>
+           </Grid>
+         </Box>):(<Modal open={open} handleClose={handleClose} ></Modal>)}
         </Box>
     );
 };
